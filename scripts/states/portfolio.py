fffb46105e514a085572bcac498e004c7eb83c05
Eze Jonas <chinweike63@gmail.com>
2026-06-22 16:41:38 +0100

    cleaned files

--- scripts/states/portfolio.py
+++
@@ -1,7 +0,0 @@
-portfolio = {
-    "starting_capital": 10000.0,
-    "cash": 10000.0,
-    "btc_holdings": 0.0,
-    "current_price": 0.0,
-    "trades": []
-}
--- scripts/values/portfolio_value.py
+++
@@ -1,7 +0,0 @@
-def portfolio_value(portfolio):
-
-    return (
-        portfolio["cash"]
-        + portfolio["btc_holdings"]
-        * portfolio["current_price"]
-    )
